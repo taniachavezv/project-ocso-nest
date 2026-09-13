bd92bf77f79baf4b2a335b0011a0c47eeb36bc49
taniachavezv <tchavez30@alumnos.uaq.mx>
2026-09-13 14:59:12 -0600

    Database config

--- src/employees/employees.service.ts
+++ src/employees/employees.service.ts
@@ -1,25 +1,26 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateEmployeeDto } from './dto/create-employee.dto.js';
 import { UpdateEmployeeDto } from './dto/update-employee.dto.js';
+import { v4 as uuid } from 'uuid';
 
 @Injectable()
 export class EmployeesService {
   private employees: CreateEmployeeDto[] = [
   {
-    id: 1,
+    id: uuid(),
     name: "Alberto",
     lastName: "Costas",
     phoneNumber: "XXX443221"
   },
   {
-    id: 2,
+    id: uuid(),
     name: "José",
     lastName: "Pérez",
     phoneNumber: "4424213XX"
   }
 ]
   create(createEmployeeDto: CreateEmployeeDto) {
-    createEmployeeDto.id = this.employees.length + 1
+    createEmployeeDto.id = uuid()
     this.employees.push(createEmployeeDto);
     return createEmployeeDto;
   }
@@ -29,17 +30,19 @@ export class EmployeesService {
     return this.employees;
   }
 
-  findOne(id: number) {
+  findOne(id: string) {
     const employee = this.employees.filter((employee)=>employee.id === id)[0];
+    if (!employee) throw new NotFoundException();
     return employee;
   }
 
-  update(id: number, updateEmployeeDto: UpdateEmployeeDto) {
+  update(id: string, updateEmployeeDto: UpdateEmployeeDto) {
     let employeeToUpdate = this.findOne(id);
     employeeToUpdate = {
       ... employeeToUpdate,
       ... updateEmployeeDto,
     }
+    if(employeeToUpdate) throw new NotFoundException
     this.employees = this.employees.map((employee)=> { 
       if(employee.id === id) {
         employee = employeeToUpdate
@@ -49,7 +52,8 @@ export class EmployeesService {
     return employeeToUpdate;
   }
 
-  remove(id: number) {
+  remove(id: string) {
+    this.findOne(id)
     this.employees = this.employees.filter((employee)=> employee.id !== id);
     return this.employees;
   }
